b2ea857870915bfbd30d7ef0c4feea5af9e6068f
Anton Abdrakhmanov <ode15s@yandex.ru>
2023-03-03 11:52:55 +0300

    Task1: is done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,3 +4,48 @@
 
 // 1, -7, 567, 89, 223-> 3
 
+int InputInt32(string message)
+{
+    System.Console.Write(($"{message} "));
+    int value;
+    bool isCorrectInt32 = int.TryParse(Console.ReadLine(), out value);
+    if (isCorrectInt32)
+    {
+        return value;
+    }
+    System.Console.WriteLine("You entered not a number");
+    Environment.Exit(-1);
+    return 0;
+}
+
+
+
+int[] UserInputArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = InputInt32("Enter you number");
+    }
+    return array;
+}
+
+int PositiveElementCounter(int[] array)
+{
+    int Count=0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) Count++;
+    }
+    if (Count!=0)
+    {
+        return Count;
+    }
+    System.Console.WriteLine("You entered no one positive number");
+    Environment.Exit(-1);
+    return -1;
+}
+
+int  UserArraySize = InputInt32("Enter the amount of your numbers");
+int[] UserArrayNumbers = UserInputArray(UserArraySize);
+if (PositiveElementCounter(UserArrayNumbers)!=0) System.Console.WriteLine($"You entered {PositiveElementCounter(UserArrayNumbers)} positive number(s)");
